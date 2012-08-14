c7368b9a0fe427da815f630a4c1a060d302aa0d4
Rachid Belaid <rachid.belaid@gmail.com>
2012-08-14 01:10:18 +0100

    update setup of blog

---
+++ pages/me.rst
@@ -0,0 +1,7 @@
+About me
+########
+
+:date: 2012-08-12 
+:author: Rach Belaid
+
+It's me
--- settings.py
+++ settings.py
@@ -13,6 +13,8 @@ SITENAME = u"There's a snake in my boot!"
 SITESUBTITLE = u"Python Charmer, Git Lover, OpenSource Admirer, Technology Dreamer"
 SITEURL = 'http://rachbelaid.com'
 RELATIVE_URLS = False
+PAGE_DIR='pages'
+
 TIMEZONE = 'Europe/London'
 GITHUB_REPO = "https://github.com/rach/rach.github.com"
 GIT_BRANCH = 'source'
@@ -22,16 +24,19 @@ TWITTER_SHARE_URL_SCHEMA = "https://twitter.com/intent/tweet?original_referer=%s
 TWITTER_SHARE_URL = TWITTER_SHARE_URL_SCHEMA % (SITEURL, TWITTER_USERNAME, TWITTER_USERNAME, "%s", "%s")
 DEFAULT_LANG='en'
 #GOOGLE_ANALYTICS = ""
-# Blogroll
-LINKS =  (
-    ('Twitter', 'https://twitter.com/rachbelaid'),
-    ('Linkedin', 'http://uk.linkedin.com/in/rachbelaid'),
-    ('Github', 'http://github.com/rach/'),
+# Social widgets
+# Name , link, new page
+SOCIAL =  (
+
+     ('Me', '%s/about-me.html'% SITEURL , False  ),
+     ('Twitter', 'https://twitter.com/rachbelaid', True),
+    ('Linkedin', 'http://uk.linkedin.com/in/rachbelaid', True),
+    ('Github', 'http://github.com/rach/', True),
          )
 
-# Social widget
-SOCIAL = (
-          ('You can add links in your config file', '#'),
+# Links
+LINKS = (
+
          )
 
 DEFAULT_PAGINATION = 7
@@ -40,11 +45,10 @@ DEFAULT_ORPHANS = 1
 DISQUS_SITENAME = "rach-belaid-blog"
 
 DEFAULT_DATE_FORMAT = "%d %b %Y"
-
 # A list of files to copy from the source to the destination
-#FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
+FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),('extra/CNAME', 'CNAME'),)
 
-
+#STATIC_PAGES = {'/me.html': 'static/me.html', }
 
 #PLUGIN_PATH = "plugins"
 #from plugins.bitly import bitly_plugin
